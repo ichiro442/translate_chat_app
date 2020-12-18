8e74866a4c2b856a8c2c845ce1c19c079a66570f
森山一郎 <moriyamaichirou@moriyamaichirounoMacBook-Air.local>
2020-12-18 15:12:38 +0900

    はじめて

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+/* import Firebase */
+import firebase from 'firebase'
+
+Vue.config.productionTip = false
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAi1beesNa-xkSikXMM1Pl3gbgxSXMcQIQ",
+    authDomain: "chat-app-bd90a.firebaseapp.com",
+    databaseURL: "https://chat-app-bd90a-default-rtdb.firebaseio.com",
+    projectId: "chat-app-bd90a",
+    storageBucket: "chat-app-bd90a.appspot.com",
+    messagingSenderId: "529935428182",
+    appId: "1:529935428182:web:21d926724357728e7629f7",
+    measurementId: "G-SH8MZZNSTE"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
+  new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
